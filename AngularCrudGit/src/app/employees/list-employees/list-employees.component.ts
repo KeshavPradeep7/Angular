d9e9d25c5f35710f8f0774bcf6bcbd4c7cc0cbb8
KeshavPradeep7 <keshavpradeepramanath@gmail.com>
2020-12-28 17:23:52 +0530

    List Employees

---
+++ AngularCrudGit/src/app/employees/list-employees/list-employees.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from 'src/app/models/employee.model';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent implements OnInit {
+
+  employees:Employee[]=[
+    {
+    id:1,
+    name:'Pradeep',
+    gender:'Male',
+    contactPreference:'email',
+    email:'pradeep@gail.com',
+    dateOfBirth:new Date('06/07/1989'),
+    department:'IT',
+    isActive:true,
+    phoneNumber:9535722138,
+    photoPath:'assets/images/Pradeep.jpg'
+    },
+
+    {
+      id:2,
+      name:'Akshatha',
+      gender:'Female',
+      contactPreference:'email',
+      email:'ashathan@gail.com',
+      dateOfBirth:new Date('18/07/1991'),
+      department:'Law',
+      isActive:true,
+      phoneNumber:9036421621,
+      photoPath:'assets/images/Loli.jpg'
+      },
+      {
+        id:3,
+        name:'Chobi',
+        gender:'Male',
+        contactPreference:'email',
+        email:'avyukth@gail.com',
+        dateOfBirth:new Date('20/05/2017'),
+        department:'Baby Sitting',
+        isActive:true,
+        photoPath:'assets/images/Chobi.jpg'
+        }
+]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
